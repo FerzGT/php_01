15c97e7c3177df5a9a719208bcd93bf8051f8461
FerzGT <[email protected]>
2025-03-03 08:23:38 +0300

    3

--- index.php
+++ index.php
@@ -3,58 +3,27 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php        
-      // $variable = 3.14;
-      // $variable = 3;
-      $variable = 'one';
-      // $variable = true;
-      // $variable = null;
-      // $variable = [];
-
-      echo 'Используем конструкцию "if..elseif" </br>';
+    <?php 
+    // 1) Выведите на экран при помощи echo() название текущего файла и номер текущей строки
+    $path = __FILE__;
+    $nameFile = basename($path) ;
+    echo "1) Название текущего файла - $nameFile </br>"; 
+    echo "Номер строки - ", __LINE__, "</br></br>";  
     
-      if (is_bool($variable)) {
-        $type = "bool";
-      } elseif (is_float($variable)) {
-        $type = "float";
-      } elseif (is_int($variable)) {
-        $type = "int";
-      } elseif (is_string($variable)) {
-         $type = "string";
-      } elseif (is_null($variable)) {
-        $type = "null";
-      } else {
-        $type = "other";
-      }
-    
-      echo "type is $type </br></br>";
+    // 2) Создайте многострочную переменную при помощи heredoc синтаксиса
+    $textHeredoc = <<<HERE
+          2) Это многострочная </br>
+          переменная, созданная при помощи </br>
+          heredoc синтаксиса </br></br>
+    HERE;
+    echo $textHeredoc;
 
-      echo <<<HERE
-        Используем конструкцию "switch-case" </br>
-        HERE;
-    
-      switch (true) {
-        case (is_bool($variable)) :
-              $type = "bool";
-              break;
-        case (is_float($variable)) :
-              $type = "float";
-              break;
-        case (is_int($variable)) :
-              $type = "int";
-              break;
-        case (is_string($variable)) :
-              $type = "string";
-              break;
-        case (is_null($variable)) :
-              $type = "null";
-              break;
-        default:
-          $type = "other";
-      }
-
-    echo "type is $type"
+    // 3) Задайте две простые строковые переменные и используйте их для построения конечной фразы
+    $a='Рыба';
+    $b='человек';
+    echo "3) $a рыбою сыта, а $b {$b}ом";
     
     ?> 
-
+  </body>
+       
 </html>
